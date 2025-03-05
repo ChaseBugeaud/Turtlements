16c7781ed49793b8518ba0f10eddfe7f8ea1bd0e
c-casey <christian.b.casey@gmail.com>
2025-03-05 12:03:30 -0800

    Initialize testing with Mocha

---
+++ test/test.js
@@ -0,0 +1,11 @@
+var assert = require('assert');
+
+describe('Array', () => {
+  describe('#indexOf()', () => {
+    context("when checking that testing works", () => {
+      it('should return -1 when the value is not present', function () {
+        assert.equal([1, 2, 3].indexOf(4), -1)
+      })
+    })
+  })
+})
