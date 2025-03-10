8db58bc292924f4c9a01c29b48a10a46db78ef31
c-casey <christian.b.casey@gmail.com>
2025-03-10 11:39:02 -0700

    Initialize testing on backend

---
+++ backend/test/test.js
@@ -0,0 +1,12 @@
+import { should } from "chai"; should()
+
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      let arr = [1,2,3]
+      let result = arr.indexOf(4)
+      result.should.equal(-1)
+    })
+  })
+})
+	
