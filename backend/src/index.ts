ca5a3c8937facbde156c8adc4dea6ff3856c8447
c-casey <christian.b.casey@gmail.com>
2025-03-24 13:31:04 -0700

    Get tournament creation API working

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,8 @@ import dotenv from "dotenv"
 import express from "express"
 import { drizzle } from "drizzle-orm/node-postgres"
 import { seed } from "drizzle-seed"
-import { contestants } from "./db/schema.ts"
+import { tournaments, contestants, sponsors, scores, matchups } from "./db/schema.ts"
+import * as schema from "./db/schema.ts"
 
 dotenv.config()
 
@@ -15,20 +16,88 @@ const pg_database = process.env.PG_DATABASE
 const app = express()
 const port = 3000
 
-const db = drizzle(`postgresql://${pg_user}:${pg_pass}@${pg_host}/${pg_database}`)
+const db = drizzle({
+  connection: {
+    connectionString: `postgresql://${pg_user}:${pg_pass}@${pg_host}/${pg_database}?ssl{"rejectUnauthorized":false}`,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  }
+})
 
-async function main() {
-    await seed(db, { contestants })
-}
+app.use(express.json())
 
 app.get("/", (req, res) => {
   res.send("Hello World!")
+})
+
+app.post("/tournaments/create", async (req, res) => {
+  try {
+    await db.transaction(async (tx) => {
+      console.log(req.body)
+      const tournamentId: number | null = await tx.insert(tournaments).values({
+        name: req.body.name,
+        description: req.body.description,
+        start_date: req.body.start_date,
+        end_date: req.body.end_date,
+        prize: req.body.prize
+      })
+        .returning({ tournament_id: tournaments.id })
+        .then(res => res[0].tournament_id ?? null)
+
+      console.log(tournamentId)
+
+      for (const contestant of req.body.contestants) {
+        await tx.insert(contestants).values({
+          name: contestant.name,
+          logo: contestant.logo,
+          seed: Number(contestant.seed),
+          tournament_id: Number(tournamentId)
+        })
+      }
+
+      if (req.body.sponsor) {
+        const sponsor: SponsorObj = req.body.sponsor
+        await tx.insert(sponsors).values({
+          name: sponsor.name,
+          description: sponsor.description,
+          thumbnail: sponsor.thumbnail,
+          header_image: sponsor.header_image,
+          tournament_id: Number(tournamentId)
+        })
+      }
+    })
+    res.send("Success!")
+  } catch (err) {
+    res.send("Error! Try again. Log: " + err)
+  }
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
+type SponsorObj = {
+  name: string,
+  description: string,
+  thumbnail: string,
+  header_image: string
+}
 
-let thing = await main()
-console.log(thing)
+type ContestantObj = {
+  name: string,
+  seed: number,
+  logo: string
+}
+
+type TournamentObj = {
+  name: string,
+  description: string,
+  start_date: Date,
+  end_date: Date,
+  prize?: string,
+
+  sponsor?: SponsorObj,
+
+  contestants: ContestantObj[]
+}
