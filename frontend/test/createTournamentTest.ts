ecd5217536e9bebd87e13511289a79df49072ece
BaileyM4556 <baileymawhinney4556@gmail.com>
2025-03-25 13:41:25 -0700

    Created tests for create tournament form

--- frontend/test/createTournamentTest.ts
+++ frontend/test/createTournamentTest.ts
@@ -1,13 +1,27 @@
 
 import { should } from "chai"; should()
-import { CreateTournamentComponent } from "../src/app/create-tournament/create-tournament.component";
-describe('random comp', function () {
-  describe('random comp', function () {
-    it('5', function () {
+import { TournamentComponent } from "../src/app/tournament/tournament.component";
+describe('Tournament Object Creation Tests', function () {
+  describe('update tournament object (title) with form details', function () {
+    it('epic', function () {
+      let t2: TournamentComponent = new TournamentComponent;
+      t2.addDetailsToTournament("epic","awesome","cool",new Date(), new Date());
+      t2.tournament.getName().test.should.equal("epic");
+    })
+  })
+  describe('add contestant with empty name field', function () {
+    it('null', function () {
 
-      let comp: CreateTournamentComponent = new CreateTournamentComponent;
-      comp.test.should.equal(5);
-
+      let t1: TournamentComponent = new TournamentComponent;
+      t1.addContestantToTournament("");
+      t1.tournament.getContestants().test.should.equal(null);
+    })
+  })
+  describe('create tournament with required field (title) not filled out', function () {
+    it('asdf', function () {
+      let t2: TournamentComponent = new TournamentComponent;
+      t2.addDetailsToTournament("","test desc","",new Date(), new Date());
+      t2.tournament.getName().test.should.equal("asdf");
     })
   })
 })
