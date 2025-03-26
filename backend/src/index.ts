06f0d55bf59973d575f8104917d058312c02ef4c
c-casey <christian.b.casey@gmail.com>
2025-03-26 12:42:49 -0700

    enable cors on backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -25,6 +25,12 @@ const db = drizzle({
   }
 })
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+
 app.use(express.json())
 
 app.get("/", (req, res) => {
